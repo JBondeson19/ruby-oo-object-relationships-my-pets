9f8be97837aaf5cec1b2e5f777018af03fb102ec
JBondeson19 <jpbond58@gmail.com>
2020-08-27 17:53:06 -0400

    almost done

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,7 @@
 Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+
+
+
+
+
+
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@now_all =[]
+  #@@owner_count = -1
+
+  def initialize (name, species="human")
+    @name = name
+    @species = species
+    @@now_all << self
+    #@@owner_count  += 1
+  end
+
+  def say_species
+   p "I am a #{species}."
+  end
+
+  def self.all
+     return @@now_all
+  end
+  
+  def self.count
+    @@now_all.length
+  end
+ 
+  def self.reset_all
+    self.all.clear
+    # binding.pry 
+  end
+
 end
+
+# john = Owner.new("John")
